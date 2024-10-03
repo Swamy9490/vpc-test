2fd27d8db31cd8ab44d05092f20a9bc98fee088c
unknown <vvdswamy7479@gmail.com>
2024-10-03 22:16:41 +0530

    vpc-test

---
+++ variables.tf
@@ -0,0 +1,15 @@
+variable "vpc_cidr" {
+    default = "10.0.0.0/16"
+}
+
+variable "common_tags" {
+    default {
+        Project = "roboshop"
+        Environment = "dev"
+        Terraform = "true"
+    }
+}
+
+variable "vpc_tags" {
+    
+}
